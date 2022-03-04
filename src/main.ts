a9b52aed8092b8aa1857c4a2db7c138b9de0cb10
Felipe Gutierrez <felipeg@ibm.com>
2022-03-04 09:09:38 -0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import * as core from '@actions/core'
-
+import * as event from './event'
+import * as version from './version'
+import * as git from './git'
+import * as github from './github'
 
 export async function run(): Promise<void> {
   try {
-    core.setOutput('release-url','https://example.com')
+    const token = core.getInput('repo-token')
+
+    const tag = event.getCreatedTag()
+    var releaseUrl = ''
+
+    if (tag && version.isSemVer(tag)) {
+      const changelog = await git.getChangesIntroducedByTag(tag)
+      releaseUrl = await github.createReleaseDraft(tag, token, changelog)
+    }
+
+    core.setOutput('release-url', releaseUrl)
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
